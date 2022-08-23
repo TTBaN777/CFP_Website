5fda1d057d0774ea1858774395480df899251c75
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-08-23 22:51:27 +0200

    Create CKH_version.py

---
+++ Python/CKH_version.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://icsoc2022.spilab.es/"
+response1 = requests.get(url).text
+#print(response1)
+soup = BeautifulSoup(response1, 'html.parser')
+divs = soup.find_all('td')
+print(divs[0])
+print('\n----------\n')
