1b5497b47b80428cc4435b78eb3cda38622b9a3e
CKHuangGH <chihkaihuang1@gmail.com>
2022-08-31 13:58:20 +0200

    test

--- Python/CKH_version.py
+++ Python/CKH_version.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url = "https://icsoc2022.spilab.es/"
 response1 = requests.get(url).text
-#print(response1)
+print(response1)
 soup = BeautifulSoup(response1, 'html.parser')
 divs = soup.find_all('td')
 print(divs[0])
